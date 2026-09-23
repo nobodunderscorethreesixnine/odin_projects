5c27f67363b3410fad66ea2d697b027c5aef7855
nobodunderscorethreesixnine <113294742+nobodunderscorethreesixnine@users.noreply.github.com>
2026-09-23 19:36:10 +0545

    fix(port) port for production

--- mini_message_board/app.js
+++ mini_message_board/app.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 const { indexRouter } = require("./routes/indexRouter.js");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // views
 app.set("views", path.join(__dirname, "views"));
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
-app.listen(port);
+app.listen(PORT);
 
 app.use((err, req, res, next) => {
   res.status(404).render("404");
